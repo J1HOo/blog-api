96641eee8742f596b4ffc61eb7a95197dc5eaa89
박지호 <businessj1ho@gmail.com>
2021-07-29 09:42:16 +0900

    articles 모듈 생성

---
+++ src/articles/articles.controller.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+import { ArticlesService } from './articles.service';
+
+@Controller('articles')
+export class ArticlesController {
+  constructor(private readonly articlesService: ArticlesService) {}
+
+  @Get()
+  getHello(): string {
+    return this.articlesService.getHello();
+  }
+}
